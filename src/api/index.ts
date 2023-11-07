d335fc6e0f5044054a7ed5fbda6e402456841708
Joona Haavisto <joonahaavisto@Joonas-MBP.elisakoti>
2023-11-07 08:00:08 +0200

    use another api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 export const fetchEuribor = async (): Promise<Euribor> => {
-  const url = 'https://api.api-ninjas.com/v1/interestrate?name=euribor'
+  const url = 'https://euribor.p.rapidapi.com/all'
   const apiKey = import.meta.env.VITE_EURIBOR_API_KEY;
   const options = {
     method: 'GET',
     headers: {
-      'X-API-Key': apiKey || '',
+      'X-RapidAPI-Key': apiKey || '',
+      'X-RapidAPI-Host': 'euribor.p.rapidapi.com',
     }
   };
 
@@ -12,13 +13,7 @@ export const fetchEuribor = async (): Promise<Euribor> => {
     const response = await fetch(url, options);
     const result = JSON.parse(await response.text());
     return {
-      rates: {
-        '1month': result['non_central_bank_rates'][1]['rate_pct'] as number,
-        '3months': result['non_central_bank_rates'][2]['rate_pct'] as number,
-        '6months': result['non_central_bank_rates'][3]['rate_pct'] as number,
-        '12months': result['non_central_bank_rates'][4]['rate_pct'] as number,
-      },
-      'lastUpdated': result['non_central_bank_rates'][0]['last_updated'] as string,
+      rates: result,
     };
   } catch (error) {
     console.error(error);
